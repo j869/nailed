a649f1f1942da817bf3f5856e5b7c4b1f1b38109
John Maher <john@maherco.com.au>
2025-04-28 10:24:17 +1000

    q

--- schema_changes.sql
+++ schema_changes.sql
@@ -22,3 +22,16 @@ SET tier = 500;
 ALTER TABLE files
 ADD COLUMN build_id INT,
 ADD COLUMN job_id INT;
+
+
+ALTER TABLE users RENAME COLUMN system_access_type TO roles;
+
+
+insert into listorder (user_id, location_used, sort_order, display_text) values (0, ‘customersstatus’, 100, 'Initial Enquiry'; 
+insert into listorder (user_id, location_used, sort_order, display_text) values (0, ‘customersstatus’, 200, 'Sales System'; 
+insert into listorder (user_id, location_used, sort_order, display_text) values (0, ‘customersstatus’, 300, 'Awaiting Deposit'; 
+insert into listorder (user_id, location_used, sort_order, display_text) values (0, ‘customersstatus’, 400, 'Permits'; 
+insert into listorder (user_id, location_used, sort_order, display_text) values (0, ‘customersstatus’, 500, 'Engineering'; 
+insert into listorder (user_id, location_used, sort_order, display_text) values (0, ‘customersstatus’, 600, 'Production Ordered'; 
+insert into listorder (user_id, location_used, sort_order, display_text) values (0, ‘customersstatus’, 700, 'Delivery Scheduled'; 
+insert into listorder (user_id, location_used, sort_order, display_text) values (0, ‘customersstatus’, 800, 'Pre-Production & Final Check'; 
